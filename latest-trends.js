2a3ebd0ae371d79ec87a00be9d5027804821c35a
jeet khattar <jeetkhattar22@gmail.com>
2025-08-31 22:54:00 +0530

    first commit

---
+++ latest-trends.js
@@ -0,0 +1,11 @@
+function afterpara(){
+    document.getElementById('afterclickpara').style.display='block';
+    document.getElementById('readmore').style.display='none';
+    document.getElementById('readless').style.display='block';
+}
+
+function lesspara(){
+    document.getElementById('readmore').style.display='block';
+    document.getElementById('afterclickpara').style.display='none';
+    document.getElementById('readless').style.display='none';
+}
